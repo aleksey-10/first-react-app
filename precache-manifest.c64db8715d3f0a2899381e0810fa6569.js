1c4d4c38666c2f3c8fa1d01e5ce53d308513c04e
aleksey-10 <alexcare1337@gmail.com>
2020-02-12 00:41:39 +0200

    Updates

---
+++ precache-manifest.c64db8715d3f0a2899381e0810fa6569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3961018cdbdafd2d897716237e151035",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "35c538ad98982d364be6",
+    "url": "/first-react-app/static/css/main.0fc15f58.chunk.css"
+  },
+  {
+    "revision": "08d9cdeedc91ea0207ae",
+    "url": "/first-react-app/static/js/2.cf88a03e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-react-app/static/js/2.cf88a03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c538ad98982d364be6",
+    "url": "/first-react-app/static/js/main.e12ee6a5.chunk.js"
+  },
+  {
+    "revision": "91f3fe1e302bd421869f",
+    "url": "/first-react-app/static/js/runtime-main.38360574.js"
+  }
+]);
